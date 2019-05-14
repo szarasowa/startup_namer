fdd9e2ca358c2b6527006133c82a961d07669e96
emil <loremipsum@szarasowa.ovh>
2019-05-14 20:16:31 +0200

    Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(  // https://api.flutter.dev/flutter/material/Scaffold-class.html
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -64,6 +67,38 @@ class RandomWordsState extends State<RandomWords> {
          }
         });
       },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+        }
+      )
     );
   }
 }
